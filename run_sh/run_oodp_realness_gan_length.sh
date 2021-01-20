58df8af65ac58e14b84f716e3ca81e78ff65cedf
zihengruan <zihengruan@163.com>
2021-01-20 13:35:58 +0800

    edit realness gan scripts for num_outcomes and G_updates controlling

--- run_sh/run_oodp_realness_gan_length.sh
+++ run_sh/run_oodp_realness_gan_length.sh
@@ -4,8 +4,8 @@ seeds="16 256 1024 2048 8192"
 # dataset_file="binary_true_smp_full_v2"
 # $1 dataset_file
 # $2 mode
-# $3 minlen
-# $4 maxlen
+# $3 num_outcomes
+# $4 G_updates
 # $5 gross_result name
 for seed in ${seeds} ; do
   python -m app.run_oodp_realnessgan_length \
@@ -13,9 +13,9 @@ for seed in ${seeds} ; do
   --seed=${seed}  \
   --D_lr=2e-5 \
   --G_lr=2e-5 \
-  --num_outcomes=5 \
+  --num_outcomes=$3 \
   --D_updates=1 \
-  --G_updates=3 \
+  --G_updates=$4 \
   --beta1=0.5 \
   --beta2=0.999 \
   --G_h_size=32 \
@@ -31,7 +31,7 @@ for seed in ${seeds} ; do
   --bert_type=bert-base-chinese \
   --dataset=smp \
   --data_file=$1 \
-  --output_dir=oodp-gan/oodp-realness_gan-smp_mode$2_minlen$3_maxlen$4_s${seed} \
+  --output_dir=oodp-gan/oodp-realness_gan-smp_mode$2_outcome$3_Gupdate$4_s${seed} \
   --do_train \
   --do_eval \
   --do_test \
@@ -39,9 +39,7 @@ for seed in ${seeds} ; do
   --feature_dim=768 \
   --G_z_dim=1024  \
   --mode=$2  \
-  --minlen=$3 \
-  --maxlen=$4 \
   --result=$5
-  rm -rf oodp-gan/oodp-realness_gan-smp_mode$2_minlen$3_maxlen$4_s${seed}/save
+  rm -rf oodp-gan/oodp-realness_gan-smp_mode$2_outcome$3_Gupdate$4_s${seed}/save
 done
 exit 0
--- run_sh/run_realness_GQOGAN_minlens.sh
+++ run_sh/run_realness_GQOGAN_minlens.sh
@@ -4,14 +4,14 @@ lengths="-1"
 # dataset_file="binary_true_smp_full_v2"
 # $1 dataset_file
 # $2 mode
-# $3 minlen
-# $4 maxlen
+# $3 num_outcomes
+# $4 G_updates
 # $5 save_path of my computer
 
 for len in ${lengths} ; do
-  bash run_sh/run_oodp_realness_gan_length.sh ${1} ${2} ${len} ${4} realness_GQOGAN_mode${2}_minlen${len}_maxlen${4}
-  mv oodp-gan realness_GQOGAN_mode${2}_minlen${len}_maxlen${4}
-  mv realness_GQOGAN_mode${2}_minlen${len}_maxlen${4}_gross_result.csv realness_GQOGAN_mode${2}_minlen${len}_maxlen${4}
-  cp -r "/content/Gan-s-Exploration/realness_GQOGAN_mode${2}_minlen${len}_maxlen${4}" "${5}"
+  bash run_sh/run_oodp_realness_gan_length.sh ${1} ${2} ${3} ${4} realness_GQOGAN_mode${2}_outcome${3}_Gupdate${4}
+  mv oodp-gan realness_GQOGAN_mode${2}_outcome${3}_Gupdate${4}
+  mv realness_GQOGAN_mode${2}_outcome${3}_Gupdate${4}_gross_result.csv realness_GQOGAN_mode${2}_outcome${3}_Gupdate${4}
+  cp -r "/content/Gan-s-Exploration/realness_GQOGAN_mode${2}_outcome${3}_Gupdate${4}" "${5}"
 done
 exit 0
