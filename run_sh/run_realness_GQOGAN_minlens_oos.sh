8d868ed6cb150eb60c9d7598218b1f5b4a8c71dd
zihengruan <zihengruan@163.com>
2021-04-10 20:01:59 +0800

    remove beta in RealnessGAN clinc150 script

--- run_sh/run_realness_GQOGAN_minlens_oos.sh
+++ run_sh/run_realness_GQOGAN_minlens_oos.sh
@@ -6,12 +6,11 @@ lengths="-1"
 # $2 num_outcomes
 # $3 G_updates
 # $4 save_path of my computer
-# $5 beta
 
 for len in ${lengths} ; do
-  bash run_sh/run_oodp_realness_gan_length_oos.sh ${1} ${2} ${3} oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_beta${5} ${4} ${5}
-  mv oodp-gan oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_beta${5}
-  mv oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_beta${5}_gross_result.csv oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_beta${5}
-  cp -r "/content/Gan-s-Exploration/oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_beta${5}" "${4}"
+  bash run_sh/run_oodp_realness_gan_length_oos.sh ${1} ${2} ${3} oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3} ${4}
+  mv oodp-gan oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}
+  mv oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}_gross_result.csv oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}
+  cp -r "/content/Gan-s-Exploration/oos-${1}-realness_GQOGAN_outcome${2}_Gupdate${3}" "${4}"
 done
 exit 0
